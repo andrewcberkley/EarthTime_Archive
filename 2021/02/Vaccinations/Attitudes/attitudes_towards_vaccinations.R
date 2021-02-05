daecff419b3a9ded4c48ca01e3dbfd1f9dfe9d91
berks <andrewcberkley@gmail.com>
2021-02-05 15:45:57 +0100

    Update attitudes_towards_vaccinations.R

--- 2021/02/Vaccinations/Attitudes/attitudes_towards_vaccinations.R
+++ 2021/02/Vaccinations/Attitudes/attitudes_towards_vaccinations.R
@@ -34,4 +34,29 @@ list.files()
 temp = list.files(pattern="*.csv")
 for (i in 1:length(temp)) assign(temp[i], read.csv(temp[i]))
 
+rm(i)
+rm(temp)
+rm(zipF)
 
+#vac_1		
+#Value
+#Standard Attributes	Label	If a Covid-19 vaccine were made available to me this week, I would definitely get it:
+#  Type	Numeric
+#Measurement	Nominal
+#Valid Values	1	1 - Strongly agree
+#2	2
+#3	3
+#4	4
+#5	5 - Strongly disagree
+
+#Conditionally remove data frames from environment
+#https://stackoverflow.com/questions/28195504/conditionally-remove-data-frames-from-environment
+to.rm <- unlist(eapply(.GlobalEnv, function(x) is.data.frame(x) && ncol(x) < 3))
+rm(list = names(to.rm)[to.rm], envir = .GlobalEnv)
+
+
+
+#https://stackoverflow.com/questions/36923182/r-remove-a-row-from-all-data-frames-in-global-environment
+dfs = sapply(ls(), is.data.frame) 
+#https://stackoverflow.com/questions/62274164/remove-same-column-from-multiple-dataframes-in-r
+dfs <- lapply(dfs, function(x) x[names(x) != "vac_1"])
