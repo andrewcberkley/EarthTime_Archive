3b8fc827bd76eb4d06481624bad00bbb7072acaf
berks <andrewcberkley@gmail.com>
2021-04-14 19:46:13 +0200

    Update nytimes_covid.R

--- 2020/01/2019-nCoV_infection/nytimes_covid.R
+++ 2020/01/2019-nCoV_infection/nytimes_covid.R
@@ -65,3 +65,6 @@ sheet_write(covid_final, ss = ss, sheet = "Sheet1")
 #The number "1" above in this code is meant to automatically respond to the following prompt:
 # The googlesheets4 package is requesting access to your Google account. Select a pre-authorised account or enter '0' to obtain a new token. Press Esc/Ctrl + C to abort.
 # 1: andrewcberkley@gmail.com
+
+rm(list = ls())
+.rs.restartR()
