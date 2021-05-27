787cd8dc1dd4dde4ae2d35daca47ef5ab67c71eb
berks <andrewcberkley@gmail.com>
2021-05-27 14:19:17 +0200

    Update PIRUS_data_wrangling.R

--- 2021/05/profiles_of_individual_radicalization_in_the_united_states/PIRUS_data_wrangling.R
+++ 2021/05/profiles_of_individual_radicalization_in_the_united_states/PIRUS_data_wrangling.R
@@ -1,5 +1,7 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/05/profiles_of_individual_radicalization_in_the_united_states'))
 
+library(tidyverse)
+
 df <- readxl::read_excel("PIRUS_Public_May2020.xlsx")
 lat_long <- read.csv("us-zip-code-latitude-and-longitude.csv", sep=";")
 
