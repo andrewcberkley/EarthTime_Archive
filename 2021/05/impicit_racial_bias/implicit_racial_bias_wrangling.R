2e451309597dafc40ae4df8a91676c4f92d904b8
berks <andrewcberkley@gmail.com>
2021-06-07 18:07:23 +0200

    Update implicit_racial_bias_wrangling.R

--- 2021/05/impicit_racial_bias/implicit_racial_bias_wrangling.R
+++ 2021/05/impicit_racial_bias/implicit_racial_bias_wrangling.R
@@ -19,13 +19,15 @@ for (fileNumber in fileNumbers)
   csvFileName <-  paste(sub(paste("\\.", "sav", sep = ""), "", shortFileName),".", "csv", sep = "")
   dataset1 = read.spss(fileNames[fileNumber], to.data.frame=TRUE)
 
-#  if (fileNames == "Race IAT.public.2002-2003.sav|Race IAT.public.2004.sav") {
-#    df <- dataset1[,c("D_biep.White_Good_all","country","ethnic")]
-#    colnames(df)[which(names(df) == "raceomb")] <- "ethnic"
-#    } else if (fileNames == "Race IAT.public.2005.sav|Race IAT.public.2006.sav")  {
-#      df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnic")] 
-#      } else {
-        df <- dataset1[,c("D_biep.White_Good_all","countrycit_num","countryres_num","ethnicityomb")]
-#        }
+  if (fileNames == "Race IAT.public.2002-2003.sav|Race IAT.public.2004.sav") {
+    df <- dataset1[,c("D_biep.White_Good_all","country","ethnic")]
+    colnames(df)[which(names(df) == "raceomb")] <- "ethnic"
+    } else if (fileNames == "Race IAT.public.2005.sav|Race IAT.public.2006.sav")  {
+      df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnic")] 
+      } else if (fileNames == "Race IAT.public.2017.sav|Race IAT.public.2018.sav|Race IAT.public.2019.sav|Race IAT.public.2020.sav")  {
+        df <- dataset1[,c("D_biep.White_Good_all","countrycit_num","countryres_num","ethnic")] 
+        }else {
+          df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnicityomb")]
+        }
   write.csv(df, csvFileName)
 }
