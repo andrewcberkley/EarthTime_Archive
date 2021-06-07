ce444e72fdc5d6eda388bc4731bcbd4d860b6c81
berks <andrewcberkley@gmail.com>
2021-06-07 18:12:13 +0200

    Update implicit_racial_bias_wrangling.R

--- 2021/05/impicit_racial_bias/implicit_racial_bias_wrangling.R
+++ 2021/05/impicit_racial_bias/implicit_racial_bias_wrangling.R
@@ -5,29 +5,49 @@ library(memisc)
 library(plyr)
 library(dplyr)
 
-fileNames <- Sys.glob(paste("implicit_bias_project_implicit_harvard_university/race_iat_public/*.", "sav", sep = ""))
-fileNumbers <- seq(fileNames)
-# Loop for..
-# 1) Iteratively get all the raw statistical file from 'raw' folder 
-# 2) Generate csv file and put under 'cleansed' folder for further processing
+## Loop for..
+## 1) Iteratively get all the raw statistical file from 'raw' folder 
+## 2) Generate csv file and put under 'cleansed' folder for further processing
+#fileNames <- Sys.glob(paste("implicit_bias_project_implicit_harvard_university/race_iat_public/*.", "sav", sep = ""))
+#fileNumbers <- seq(fileNames)
+#memory.limit(size=407070)
+#setwd(file.path(Sys.getenv('my_dir'),'2021/05/impicit_racial_bias/cleaned_datea_race_iat_public/'))
+#for (fileNumber in fileNumbers)
+#{
+#  shortFileName <- tail(strsplit(fileNames[fileNumber],"/")[[1]],1)
+#  csvFileName <-  paste("cleaned_datea_race_iat_public/", sub(paste("\\.", "sav", sep = ""), "", shortFileName),".", "csv", sep = "")
+#  dataset1 = read.spss(fileNames[fileNumber], to.data.frame=TRUE)
 
-memory.limit(size=407070)
+#  if (fileNames == "Race IAT.public.2002-2003.sav|Race IAT.public.2004.sav") {
+#    df <- dataset1[,c("D_biep.White_Good_all","country","ethnic")]
+#    colnames(df)[which(names(df) == "raceomb")] <- "ethnic"
+#    } else if (fileNames == "Race IAT.public.2005.sav|Race IAT.public.2006.sav")  {
+#      df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnic")] 
+#      } else if (fileNames == "Race IAT.public.2017.sav|Race IAT.public.2018.sav|Race IAT.public.2019.sav|Race IAT.public.2020.sav")  {
+#        df <- dataset1[,c("D_biep.White_Good_all","countrycit_num","countryres_num","ethnic")] 
+#        }else {
+#          df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnicityomb")]
+#        }
+#  write.csv(df, csvFileName)
+#}
+
+###### cleaning the csv files for unwanted entries in countrycit ######
+setwd(file.path(Sys.getenv('my_dir'),'2021/05/impicit_racial_bias/cleaned_datea_race_iat_public/'))
+
+fileNames <- Sys.glob(paste(dataloc,"/cleansed/*.", "csv", sep = ""))
+fileNumbers <- seq(fileNames)
 
 for (fileNumber in fileNumbers)
 {
-  shortFileName <- tail(strsplit(fileNames[fileNumber],"/")[[1]],1)
-  csvFileName <-  paste(sub(paste("\\.", "sav", sep = ""), "", shortFileName),".", "csv", sep = "")
-  dataset1 = read.spss(fileNames[fileNumber], to.data.frame=TRUE)
-
-  if (fileNames == "Race IAT.public.2002-2003.sav|Race IAT.public.2004.sav") {
-    df <- dataset1[,c("D_biep.White_Good_all","country","ethnic")]
-    colnames(df)[which(names(df) == "raceomb")] <- "ethnic"
-    } else if (fileNames == "Race IAT.public.2005.sav|Race IAT.public.2006.sav")  {
-      df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnic")] 
-      } else if (fileNames == "Race IAT.public.2017.sav|Race IAT.public.2018.sav|Race IAT.public.2019.sav|Race IAT.public.2020.sav")  {
-        df <- dataset1[,c("D_biep.White_Good_all","countrycit_num","countryres_num","ethnic")] 
-        }else {
-          df <- dataset1[,c("D_biep.White_Good_all","countrycit","countryres","ethnicityomb")]
-        }
-  write.csv(df, csvFileName)
+  #csvFileName <-  paste(sub(paste("\\.", "csv", sep = ""), "", fileNames[fileNumber]),".", "csv", sep = "")
+  df = read.csv(file.path(fileNames[fileNumber]), header=TRUE)
+  df2 <- df[!df$countrycit %in% c(". ","  ",".",".   ","    "),]
+  ## Remove the white spaces infront of countrycodes ##
+  df22 <- data.frame(lapply(df2, trimws))
+  ## removing an unwanted column carried over
+  cols.dont.want <- "X"
+  df3 <- df22[, ! names(df22) %in% cols.dont.want, drop = F]
+  xx<-fileNames[fileNumber]
+  #xx <- paste(dataloc,"/cleansed/",tail(strsplit(fileNames[fileNumber],"/")[[1]],1),sep="")
+  write.csv(df3, xx, row.names=FALSE, quote=FALSE)
 }
