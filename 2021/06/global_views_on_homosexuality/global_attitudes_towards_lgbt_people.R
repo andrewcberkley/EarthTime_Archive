210a31081b0ca2fd094227e08412e193a3ebe9d9
berks <andrewcberkley@gmail.com>
2021-06-10 14:35:48 +0200

    Create global_attitudes_towards_lgbt_people.R

---
+++ 2021/06/global_views_on_homosexuality/global_attitudes_towards_lgbt_people.R
@@ -0,0 +1,23 @@
+options(java.parameters = "-Xmx12000m")
+library(tabulizer)
+#Error: package or namespace load failed for 'tabulizer':
+#  .onLoad failed in loadNamespace() for 'tabulizerjars', details:
+#  call: NULL
+#error: .onLoad failed in loadNamespace() for 'rJava', details:
+#  call: fun(libname, pkgname)
+#error: JAVA_HOME cannot be determined from the Registry
+library(rJava)
+#Error: package or namespace load failed for 'rJava':
+#  .onLoad failed in loadNamespace() for 'rJava', details:
+#  call: fun(libname, pkgname)
+#error: JAVA_HOME cannot be determined from the Registry
+
+#rJava install error "JAVA_HOME cannot be determined from the Registry"
+#https://stackoverflow.com/questions/29522088/rjava-install-error-java-home-cannot-be-determined-from-the-registry
+
+#https://www.gzeromedia.com/the-graphic-truth-global-attitudes-towards-lgbt-people
+#https://www.pewresearch.org/global/2020/06/25/global-divide-on-homosexuality-persists/
+#https://www.pewresearch.org/global/wp-content/uploads/sites/2/2020/06/PG_2020.06.25_Global-Views-Homosexuality_TOPLINE.pdf
+#https://www.pewresearch.org/methodology/international-survey-research/international-methodology/all-survey/all-country/all-year
+
+#In Japan, where conservative ideas about gender identity and sexual orientation have long dominated, 68% of Japanese now think gay people should be fully accepted by society.That's an increase of 14 percentage points from 2013.
