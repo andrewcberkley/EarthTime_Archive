6c2f196769327ff62b6d973fc9734ca6cb79b47c
berks <andrewcberkley@gmail.com>
2021-06-11 13:43:40 +0200

    Update historical_american_lynching_data_wrangling.R

--- 2021/06/historical_american_lynching_data_collection_project/historical_american_lynching_data_wrangling.R
+++ 2021/06/historical_american_lynching_data_collection_project/historical_american_lynching_data_wrangling.R
@@ -10,9 +10,23 @@ df$Race[df$Race == "Blk"] <-  "Black"
 df$Race[df$Race == "Wht"] <-  "White"
 df$Race[df$Race == "Unk"] <-  "Unknown"
 HAL <- df[,c(1,2,6,7,8,10)]
+HAL <- HAL[!grepl("Indeterminant", HAL$County),]
+HAL <- HAL[!grepl("Undetermined", HAL$County),]
 rm(df)
 
 as.data.frame(table(HAL$Race))
 as.data.frame(table(HAL$State))
 as.data.frame(table(HAL$Sex))
+as.data.frame(table(HAL$Year))
+
+HAL$Coordinates <- paste0(HAL$County,"-",HAL$State)
+HAL$Latitude <- NA
+HAL$Longitude <- NA
+us_county_centroids$Coordinates <- paste0(us_county_centroids$county,"-",us_county_centroids$state)
+us_county_centroids$Coordinates <- gsub(" County", "", us_county_centroids$Coordinates)
+us_county_centroids$Coordinates <- gsub(" Parish", "", us_county_centroids$Coordinates)
+
+HAL$Latitude <- us_county_centroids[match(HAL$Coordinates, us_county_centroids$Coordinates), 4]
+HAL$Longitude <- us_county_centroids[match(HAL$Coordinates, us_county_centroids$Coordinates), 3]
+rm(us_county_centroids)
 
