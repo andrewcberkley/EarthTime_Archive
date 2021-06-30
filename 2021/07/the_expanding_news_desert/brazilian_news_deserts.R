df3eb475982b5e9d44d35c2b9b43f5f7e38a39a0
berks <andrewcberkley@gmail.com>
2021-06-30 18:09:39 +0200

    Update brazilian_news_deserts.R

--- 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
+++ 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
@@ -1,6 +1,6 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/07/the_expanding_news_desert'))
 
-if (!require("devtools")) install.packages("devtools")
-devtools::install_github("voltdatalab/newsatlasbr")
+#if (!require("devtools")) install.packages("devtools")
+#devtools::install_github("voltdatalab/newsatlasbr")
 
 library(newsatlasbr)
