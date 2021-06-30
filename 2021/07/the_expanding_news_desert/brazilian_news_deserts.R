319bd80f2327d2064eca00885bf11e345a0c853e
berks <andrewcberkley@gmail.com>
2021-06-30 18:59:43 +0200

    Update brazilian_news_deserts.R

--- 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
+++ 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
@@ -4,3 +4,5 @@ setwd(file.path(Sys.getenv('my_dir'),'2021/07/the_expanding_news_desert'))
 #devtools::install_github("voltdatalab/newsatlasbr")
 
 library(newsatlasbr)
+
+#https://latamjournalismreview.org/news/digital-journalism-reduces-incidence-of-news-deserts-in-brazil/
