1109b6dfca46398f4e5d902370fd486a27801591
berks <andrewcberkley@gmail.com>
2021-07-06 19:52:12 +0200

    Update PIRUS_dotmap_creation.py

--- 2021/05/profiles_of_individual_radicalization_in_the_united_states/PIRUS_dotmap_creation.py
+++ 2021/05/profiles_of_individual_radicalization_in_the_united_states/PIRUS_dotmap_creation.py
@@ -15,27 +15,35 @@ def PackColor(color):
 def hex2rgb(h):
   return tuple(int(h.strip("#")[i:i+2], 16) for i in (0, 2 ,4))
 
-raw_data = []
-with open("HAL_final_black.csv", encoding="utf8") as f:
-  reader = csv.DictReader(f, delimiter=",")
-  for row in reader:
-    raw_data.append(row)
+def multiple_variables(csv_name, longitude_name, latitude_name, rgb_color_scheme, date_name, date_format, bin_name):
+  raw_data = []
+  with open(csv_name, encoding="utf8") as f:
+    reader = csv.DictReader(f, delimiter=",")
+    for row in reader:
+      raw_data.append(row)
 
-len(raw_data)
-
-raw_data[0]
+  len(raw_data)
+  raw_data[0]
 
 # rev 1
 # x,y,size_value,epoch
 # show all points in same color. Initial date at full size. after n days begin to fade dot until a year as elapsed...
 # don't distinguish between events with 0 or > 0 number of events
 #This is for **ALL** events
-points = []
-for row in raw_data:
-  x,y = LonLatToPixelXY([float(row['Longitude']), float(row['Latitude'])])
-  points.append(x)
-  points.append(y)
-  #points.append(math.sqrt(float(row['Scale']) + 1.0))
-  points.append(PackColor([255,0,0]))    
-  points.append(FormatEpoch(row["Date"], '%Y-%m'))
-array.array('f', points).tofile(open('HAL_final_black.bin', 'wb'))
+  points = []
+  for row in raw_data:
+    x,y = LonLatToPixelXY([float(row[longitude_name]), float(row[latitude_name])])
+    points.append(x)
+    points.append(y)
+    #points.append(math.sqrt(float(row['Scale']) + 1.0))
+    points.append(PackColor([rgb_color_scheme]))    
+    points.append(FormatEpoch(row[date_name], date_format))
+  array.array('f', points).tofile(open(bin_name, 'wb'))
+
+#Far-Right=#1ECBE1
+#Far-Left=#E11ECB
+#Radical Islamist=#CBE11E
+
+#Low-Income=#7224DB
+#Middle-Income=#DB7224
+#High-Income=#24DB72
