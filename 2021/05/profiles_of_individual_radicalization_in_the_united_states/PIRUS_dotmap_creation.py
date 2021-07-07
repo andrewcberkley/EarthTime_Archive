d506377869161fea77d05b22ef8125af98e001ae
berks <andrewcberkley@gmail.com>
2021-07-07 15:32:59 +0200

    Update PIRUS_dotmap_creation.py

--- 2021/05/profiles_of_individual_radicalization_in_the_united_states/PIRUS_dotmap_creation.py
+++ 2021/05/profiles_of_individual_radicalization_in_the_united_states/PIRUS_dotmap_creation.py
@@ -1,39 +1,43 @@
-import array, csv, math, os, time
-from datetime import timedelta, date, datetime
+import array, calendar, csv, math, time
 
-def FormatDateStr(date_str, format_str):
-    return time.mktime(time.strptime(date_str, format_str))
+def LonLatToPixelXY(lonlat):
+  (lon, lat) = lonlat
+  x = (lon + 180.0) * 256.0 / 360.0
+  y = 128.0 - math.log(math.tan((lat + 90.0) * math.pi / 360.0)) * 128.0 / math.pi
+  return [x, y]
 
-def LngLatToWebMercator(lnglat):
-    (lng, lat) = lnglat
-    x = (lng + 180.0) * 256.0 / 360.0
-    y = 128.0 - math.log(math.tan((lat + 90.0) * math.pi / 360.0)) * 128.0 / math.pi
-    return [x, y]
+def FormatEpoch(datestr, formatstr):
+  return calendar.timegm(time.strptime(datestr, formatstr))
 
+def PackColor(color):    
+  return color[0] + color[1] * 256.0 + color[2] * 256.0 * 256.0;
 
-def PackColor(color):
-    return color[0] + color[1] * 256.0 + color[2] * 256.0 * 256.0;
+def hex2rgb(h):
+  return tuple(int(h.strip("#")[i:i+2], 16) for i in (0, 2 ,4))
 
 def multiple_variables(csv_name, longitude_name, latitude_name, rgb_color_scheme, date_name, date_format, bin_name):
   raw_data = []
-  with open(csv_name) as f:
+  with open(csv_name, encoding="utf8") as f:
     reader = csv.DictReader(f, delimiter=",")
     for row in reader:
       raw_data.append(row)
 
   len(raw_data)
-
   raw_data[0]
 
-
-  #format x,y,packed_color,epoch_0,epoch_1
+  # rev 1
+  # x,y,size_value,epoch
+  # show all points in same color. Initial date at full size. after n days begin to fade dot until a year as elapsed...
+  # don't distinguish between events with 0 or > 0 number of events
+  #This is for **ALL** events
   points = []
   for row in raw_data:
-    x,y = LngLatToWebMercator([float(row[longitude_name]), float(row[latitude_name])])
-    packedColor = PackColor(rgb_color_scheme)
-    epoch_0 = FormatDateStr(row[date_name], date_format)
-    epoch_1 = epoch_0 + 60*60*24*28
-    points += [x,y,packedColor,epoch_0,epoch_1]
+    x,y = LonLatToPixelXY([float(row[longitude_name]), float(row[latitude_name])])
+    points.append(x)
+    points.append(y)
+    points.append(math.sqrt(float(row['Number']) + 1.0))
+    points.append(PackColor(rgb_color_scheme))    
+    points.append(FormatEpoch(row[date_name], date_format))
   array.array('f', points).tofile(open(bin_name, 'wb'))
 
 #Far-Right=#1ECBE1
