f2d5ecbdc864050a4e6c8add22ac0cfe6f73c191
berks <andrewcberkley@gmail.com>
2021-07-12 18:06:06 +0200

    Update brazilian_news_deserts.R

--- 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
+++ 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
@@ -1,12 +1,12 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/07/the_expanding_news_desert'))
 
-#if (!require("devtools")) install.packages("devtools")
-#devtools::install_github("voltdatalab/newsatlasbr")
+##if (!require("devtools")) install.packages("devtools")
+##devtools::install_github("voltdatalab/newsatlasbr")
 
 #library(newsatlasbr)
 
-#https://latamjournalismreview.org/news/digital-journalism-reduces-incidence-of-news-deserts-in-brazil/
-#https://www.atlas.jor.br/plataforma/api/pacote/
+##https://latamjournalismreview.org/news/digital-journalism-reduces-incidence-of-news-deserts-in-brazil/
+##https://www.atlas.jor.br/plataforma/api/pacote/
 
 #atlas_signin(email = Sys.getenv('atlas_da_noticia_user'), password = Sys.getenv('atlas_da_noticia_password'))
 
