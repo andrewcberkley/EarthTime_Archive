06a5fb1755206c77652867406a829a88063b58f8
berks <andrewcberkley@gmail.com>
2021-07-12 17:44:40 +0200

    Update brazilian_news_deserts.R

--- 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
+++ 2021/07/the_expanding_news_desert/brazilian_news_deserts.R
@@ -3,12 +3,16 @@ setwd(file.path(Sys.getenv('my_dir'),'2021/07/the_expanding_news_desert'))
 #if (!require("devtools")) install.packages("devtools")
 #devtools::install_github("voltdatalab/newsatlasbr")
 
-library(newsatlasbr)
+#library(newsatlasbr)
 
 #https://latamjournalismreview.org/news/digital-journalism-reduces-incidence-of-news-deserts-in-brazil/
 #https://www.atlas.jor.br/plataforma/api/pacote/
-atlas_signin(email = Sys.getenv('atlas_da_noticia_user'), password = Sys.getenv('atlas_da_noticia_password'))
 
-brazil <- get_municipalities()
-almost_deserts_dataset <- almost_deserts()
-municipalities_with_media <- n_orgs_100k()
+#atlas_signin(email = Sys.getenv('atlas_da_noticia_user'), password = Sys.getenv('atlas_da_noticia_password'))
+
+#brazil <- get_municipalities()
+#almost_deserts_dataset <- almost_deserts()
+#municipalities_with_media <- n_orgs_100k()
+
+#save(brazil, almost_deserts_dataset, almost_deserts_dataset, file = "brazil_news_deserts.RData")
+load("brazil_news_deserts")
