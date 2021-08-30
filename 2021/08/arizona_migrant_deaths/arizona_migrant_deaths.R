85f125241090acd92200bc93ebf6d2063aaa19a5
berks <andrewcberkley@gmail.com>
2021-08-30 15:31:08 +0200

    Update arizona_migrant_deaths.R

--- 2021/08/arizona_migrant_deaths/arizona_migrant_deaths.R
+++ 2021/08/arizona_migrant_deaths/arizona_migrant_deaths.R
@@ -3,6 +3,8 @@ setwd(file.path(Sys.getenv('my_dir'),'2021/08/arizona_migrant_deaths/'))
 suppressWarnings(ogis_migrant_deaths <- readxl::read_excel("ogis_migrant_deaths.xlsx"))
 
 clean_df <- ogis_migrant_deaths[,c(1,5,18,19)]
+clean_df$Dummy_Number <- 10
+write.csv(clean_df, "arizona_migrant_deaths.csv", row.names = FALSE, na = "")
 
 library(reticulate)
 #suppressWarnings(use_python("C:/ProgramData/Anaconda3/", required = TRUE))
