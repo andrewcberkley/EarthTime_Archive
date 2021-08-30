5d483e3ce404ecf945c54aa0c703d866306e4336
berks <andrewcberkley@gmail.com>
2021-08-30 15:27:29 +0200

    Update arizona_migrant_deaths.R

--- 2021/08/arizona_migrant_deaths/arizona_migrant_deaths.R
+++ 2021/08/arizona_migrant_deaths/arizona_migrant_deaths.R
@@ -2,10 +2,12 @@ setwd(file.path(Sys.getenv('my_dir'),'2021/08/arizona_migrant_deaths/'))
 
 suppressWarnings(ogis_migrant_deaths <- readxl::read_excel("ogis_migrant_deaths.xlsx"))
 
+clean_df <- ogis_migrant_deaths[,c(1,5,18,19)]
+
 library(reticulate)
 #suppressWarnings(use_python("C:/ProgramData/Anaconda3/", required = TRUE))
 #py_config()
 #py_install("pandas")
 
-source_python('PIRUS_dotmap_creation.py')
+source_python('arizona_migrant_deaths_dotmap_creation.py')
 #py_last_error()
