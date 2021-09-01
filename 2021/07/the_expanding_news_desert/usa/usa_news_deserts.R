7140c9dba7edcfb7cb4dbcfa43098b2490f58d2c
berks <andrewcberkley@gmail.com>
2021-09-01 14:05:33 +0200

    Update usa_news_deserts.R

--- 2021/07/the_expanding_news_desert/usa/usa_news_deserts.R
+++ 2021/07/the_expanding_news_desert/usa/usa_news_deserts.R
@@ -1,2 +1,5 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/07/the_expanding_news_desert/usa'))
 
+usa_news_desert <- read.csv("news_desert_columbia_journalism_review.csv")
+
+usa_news_desert <- usa_news_desert[,c(6,3,4,9,10)]
