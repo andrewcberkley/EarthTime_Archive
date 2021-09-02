7b35cb1ae54c197b07a300f806a2f0d9ea2d5bef
berks <andrewcberkley@gmail.com>
2021-09-02 10:20:32 +0200

    Update brazilian_news_deserts.R

--- 2021/07/the_expanding_news_desert/brazil/brazilian_news_deserts.R
+++ 2021/07/the_expanding_news_desert/brazil/brazilian_news_deserts.R
@@ -36,8 +36,9 @@ colnames(brazil_news_deserts)[1] <- "GEOCODIGO"
 
 all_brazil_municipalities <- read.csv("brazil_municipalities.csv", encoding="UTF-8")
 all_brazil_municipalities$GEOCODIGO <- as.character(all_brazil_municipalities$GEOCODIGO)
+colnames(all_brazil_municipalities) <- c("GEOCODIGO", "municipio", "uf")
 
-inner_joined_df <- dplyr::inner_join(brazil_news_deserts, all_brazil_municipalities, by = "GEOCODIGO")
+full_joined_df <- dplyr::full_join(brazil_news_deserts, all_brazil_municipalities, by = "GEOCODIGO")
 
 number_of_news_orgs <- brazil_news_deserts[,c(1,5,5)]
 colnames(number_of_news_orgs) <- c("GEOCODIGO", "2020", "2021")
