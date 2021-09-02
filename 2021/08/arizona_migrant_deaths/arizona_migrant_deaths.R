1978f3d8b60bfe4c7deed155717d0908a290be31
berks <andrewcberkley@gmail.com>
2021-09-02 15:09:14 +0200

    Update arizona_migrant_deaths.R

--- 2021/08/arizona_migrant_deaths/arizona_migrant_deaths.R
+++ 2021/08/arizona_migrant_deaths/arizona_migrant_deaths.R
@@ -12,6 +12,8 @@ clean_df$`Reporting Date` <- as.Date(clean_df$`Reporting Date`)
 clean_df$Dummy_Number <- 10
 final_df <- na.omit(clean_df)
 
+as.data.frame(dplyr::arrange(dplyr::desc(table(ogis_migrant_deaths$`Cause of Death`))))
+
 write.csv(final_df, "arizona_migrant_deaths.csv", row.names = FALSE, na = "")
 
 source_python('arizona_migrant_deaths_dotmap.py')
