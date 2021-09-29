e7a89596d7ce6f45f76179bc1afc4f61e083edd4
berks <andrewcberkley@gmail.com>
2021-09-29 15:39:34 +0200

    Update un_blue_helmets.R

--- 2021/10/un_blue_helmets/un_blue_helmets.R
+++ 2021/10/un_blue_helmets/un_blue_helmets.R
@@ -1,5 +1,7 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/10/un_blue_helmets'))
 
 casualties <- read.csv("odp_noticas.csv")
-contributions_rank <- read.csv("odp_contributionranks.csv")
+#contributions_rank <- read.csv("odp_contributionranks.csv")
 contributions_gender <- read.csv("odp_contributionsbygender.csv")
+
+as.data.frame(table(contributions_gender$Contributing_Country))
