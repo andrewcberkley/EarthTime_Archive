2b6f0e0e8b6b5ab7637dfa0a9928e66ca4763899
berks <andrewcberkley@gmail.com>
2021-10-07 11:02:13 +0200

    Update air_pollution_in_switzerland.R

--- 2021/10/air_pollution_in_switzerland/air_pollution_in_switzerland.R
+++ 2021/10/air_pollution_in_switzerland/air_pollution_in_switzerland.R
@@ -1,2 +1,5 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/10/air_pollution_in_switzerland'))
 
+pm25 <- read.csv("PM2.5.csv", header=FALSE)
+pm25_clean <- data.frame(do.call('rbind', strsplit(as.character(pm25$V1),';',fixed=TRUE)))
+pm25
