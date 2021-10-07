a6c2588a9f1af11abe382f68f1cdb47d7f067949
berks <andrewcberkley@gmail.com>
2021-10-07 11:19:16 +0200

    Update air_pollution_in_switzerland.R

--- 2021/10/air_pollution_in_switzerland/air_pollution_in_switzerland.R
+++ 2021/10/air_pollution_in_switzerland/air_pollution_in_switzerland.R
@@ -1,5 +1,6 @@
 setwd(file.path(Sys.getenv('my_dir'),'2021/10/air_pollution_in_switzerland'))
 
-pm25 <- read.csv("PM2.5.csv", header=FALSE)
-pm25_clean <- data.frame(do.call('rbind', strsplit(as.character(pm25$V1),';',fixed=TRUE)))
-pm25
+df1 <- read.csv("PM2.5.csv", header=FALSE)
+df2 <- data.frame(do.call('rbind', strsplit(as.character(df1$V1),';',fixed=TRUE)))
+df3 <- df2[-c(1:6),]
+colnames(df3) <- c("Date","Bern-Bollwerk","Lausanne-César-Roux","Lugano-Università","Zürich-Kaserne","Basel-Binningen","Dübendorf-Empa","Härkingen-A1","Sion-Aéroport-A9","Magadino-Cadenazzo","Payerne","Tänikon","Beromünster","Rigi-Seebodenalp","Jungfraujoch")
